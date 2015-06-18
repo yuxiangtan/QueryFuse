006c8a9201b509b6000866a955469761f02952c5
Yann <yannpaul@bu.edu>
2015-06-18 11:00:52 -0400

    errors in dynamically generated wrapper script

--- conda_packages/queryfuse/build.sh
+++ conda_packages/queryfuse/build.sh
@@ -2,12 +2,12 @@
 
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/lib/QueryFuse
-
-cat <<EOF > $PREFIX/bin/qf_wrapper
+#> $PREFIX/bin/qf_wrapper
+echo <<EOF > foo.bar 
 #!/bin/env bash
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-QF_DIR=$( cd $DIR/../lib/QueryFuse && pwd )
-python $QF_DIR/QF_multi_query_wrapper.py $@
+SCRIPT_DIR=\$( cd "\$( dirname "\${BASH_SOURCE[0]}" )" && pwd )
+QF_DIR=\$( cd \$SCRIPT_DIR/../lib/QueryFuse && pwd )
+python \$QF_DIR/QF_multi_query_wrapper.py \$@
 EOF
 chmod +x $PREFIX/bin/qf_wrapper
 cp ./QueryFuse_v1/* $PREFIX/lib/QueryFuse/
