e540beb652cc067ae55e39ac9158d90489887e83
Yann <yannpaul@bu.edu>
2015-06-18 10:51:14 -0400

    adding conda scripts, moving misspelled file

---
+++ conda_packages/queryfuse/build.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/lib/QueryFuse
+
+cat <<EOF > $PREFIX/bin/qf_wrapper
+#!/bin/env bash
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+QF_DIR=$( cd $DIR/../lib/QueryFuse && pwd )
+python $QF_DIR/QF_multi_query_wrapper.py $@
+EOF
+chmod +x $PREFIX/bin/qf_wrapper
+cp ./QueryFuse_v1/* $PREFIX/lib/QueryFuse/
