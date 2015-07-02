5fbd9e2859f3e97423a69426a2d2be7181998891
yuxiang tan <yuxiang.tan@gmail.com>
2015-07-02 15:49:07 -0400

    I modified the qf_wrapper to QF_wrapper in the build.sh and yaml file.

--- conda_packages/queryfuse/build.sh
+++ conda_packages/queryfuse/build.sh
@@ -9,5 +9,5 @@ SCRIPT_DIR=\$( cd "\$( dirname "\${BASH_SOURCE[0]}" )" && pwd )
 QF_DIR=\$( cd \$SCRIPT_DIR/../lib/QueryFuse && pwd )
 python \$QF_DIR/QF_multi_query_wrapper.py \$@
 EOF
-chmod +x $PREFIX/bin/qf_wrapper
+chmod +x $PREFIX/bin/QF_wrapper
 cp ./QueryFuse_v1/* $PREFIX/lib/QueryFuse/
